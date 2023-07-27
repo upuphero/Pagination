448417b7ebd2b291b7d213261267ca0f93cfcd3c
Zijian Ye <540864988@qq.com>
2023-07-27 11:46:49 -0400

    2023.7.27

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  selected = '5';
+  totalItems = 0;
+  pageEvent: PageEvent;
+
+  constructor(private http: HttpClient) { 
+    this.pageEvent = new PageEvent();
+  }
+
+  ngOnInit() {
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/todos').subscribe(data => {
+  console.log(data);
+  this.totalItems = data.length;
+}, error => {
+  console.log('Error: ', error);
+});
+
+
+  }
+
+  firstInterval() {
+    // Logic to go to first interval
+  }
+
+  previousInterval() {
+    // Logic to go to previous interval
+  }
+
+  nextInterval() {
+    // Logic to go to next interval
+  }
+
+  lastInterval() {
+    // Logic to go to last interval
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatPaginatorModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
